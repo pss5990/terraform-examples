e4d9fc3d00da7fc925dad96c1bc39a58e0daf89f
pss5990 <pss5990@gmail.com>
2020-11-26 13:04:31 +0000

    Update provider.tf

--- tf-config-sixth/provider.tf
+++ tf-config-sixth/provider.tf
@@ -8,7 +8,7 @@ terraform {
 
 provider "google" {
   version     = "3.5.0"
-  credentials = file("../../keys/loans-project-editor-sa.json")
+//  credentials = file("../../keys/loans-project-editor-sa.json")
   project     = var.project_id
   region      = var.project_default_region
   zone        = var.project_default_zone
